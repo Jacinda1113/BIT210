e8c281b64ed11e431723f132ade5ad1ab03e98a4
jiachintang <85091432+jiachintang@users.noreply.github.com>
2022-04-12 01:44:40 +0800

    Add files via upload

--- registerForOrg.php
+++ registerForOrg.php
@@ -1,4 +1,3 @@
-<?php session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,13 +15,13 @@
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.0/css/boxicons.min.css" rel='stylesheet'>
   <link href="assets/css/style.css" rel="stylesheet">
+
 </head>
   
 <body>
@@ -47,79 +46,67 @@
         </div>
     </header><!-- End Header -->
 
+    <?php
+      //create database connection
+      require("dbConnection.php");
+
+      if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        if(isset($_POST["orgName"]) && isset($_POST["address"])){
+            $orgName = $_POST["orgName"];
+            $orgAddress = $_POST["address"];
+
+        //retrieve all organization data
+        $duplicateQuery = "SELECT  orgName FROM organization WHERE orgName = '$orgName'";
+        $duplicate = $con->query($duplicateQuery);
+        $fetchDuplicate = $duplicate->fetch_all(MYSQLI_ASSOC);
+
+        if(count($fetchDuplicate) != 0){
+          echo"<script>alert('Organization has existed')</script>";
+        }
+        else{
+          $insertNewQuery = "INSERT INTO organization (OrgName, OrgAddress) VALUES ('$orgName', '$orgAddress')";
+          $insertNew = $con -> query($insertNewQuery);
+          if($insertNew == TRUE){
+            echo"<script>alert('Successfully Registered');window.location.href= 'admin.php';</script>";
+            
+          }
+
+        }
+      }
+    }
+    $con->close();
+    ?>
+
     <main id="main">
       <section class="inner-page">
         <h2 class="text-center">Register Organization</h2>
         <div class="container">
           <div class="card center-form">
             <div class="card-body">
-
-            <?php
-              //create database connection
-              require('dbconnect.php');
-
-              $org_name = $_POST["name"];
-	            $org_address = $_POST["address"];
-
-              // Do not register if organization already exists
-              $sqlQuery = "select name from org";
-              $flag = 0;
-              // Execute Query
-              $ret = $con->query($sqlQuery);
-              if($ret == TRUE) {
-                while ( $row = $ret->fetch_assoc() )
-                {
-                  if($org_name == $row["name"])
-                    $flag = 1; // org already exists in the database
-                }
-
-              }
-              else
-                echo " Query execution not successful";
-              if ($flag == 1 ) {
-                echo " Organisation " . $org_name . " already registered";
-
-              } else {
-              // Formulate query
-                $sqlQuery = "insert into org (name,address) values ('$org_name','$org_address')";
-
-                // Execute Query
-                $ret = $con->query($sqlQuery);
-                if($ret == TRUE)
-                  echo " One row inserted successfully";
-                else
-                  echo " Query execution not successful";
-              }
-              
-              if ($con->query($sqlQuery) === TRUE) {
-                echo "New organisation inserted successfully";
-              } else {
-                echo "Error: " . $sqlQuery . "<br>" . $con->error;
-              }
-              $con->close();
-              ?>
-            <form method="post" id="orgForm" class="mt-4" action="registerForOrg.php" >
+            <form method="post" id="orgForm" class="mt-4" action="registerForOrg.php">
               <div class="row">
                 <div class="col-3 form-group">
                   <label for="orgName" class="control-label">Organization Name: </label>
                 </div>
                 <div class="col-9">
-                  <input type="text" name="orgName" class="form-control" id="name">
+                  <input type="text" name="orgName" class="form-control" id="orgName">
                 </div>
               </div>
               
               <div class="row">
                 <div class="col-3 form-group">
-                  <label for="orgAddress" class="control-label">Address: </label>
+                  <label for="address" class="control-label">Address: </label>
                 </div>
                 <div class="col-9 form-group">
-                  <textarea class="form-control" name="address" id="address" ></textarea>
+                  <textarea class="form-control" id="address" name="address"></textarea>
                 </div>
               </div>
               
               <div class="row">
                 <div class="form-group mt-3 mt-md-0">
-                  <input type="submit" value="Submit" class="btn btn-default float-end">
+                  
+                  <a href="admin.php"><input type="submit" value="Submit" class="btn btn-default float-end"></a>
                 </div>
               </div>
             </form>
@@ -170,17 +157,14 @@
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Bootstrap javascript -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
+    <!-- Javascript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="assets/js/main.js"></script>
   <!--Register organization form validation-->
   <script>
     const orgForm = document.getElementById("orgForm");
@@ -188,8 +172,7 @@
     orgForm.addEventListener("submit", (e) => {
       e.preventDefault();
       if(validateOrgForm() == true){
-        alert("Organization has been registered successfully!");
-        orgForm.reset();
+        orgForm.submit();
       }
     });
 
